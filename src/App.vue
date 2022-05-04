9a17712246ea357fa5a1f5dfd6203bc4eb0ba56c
abolfazl-hue <abolfazljdst@gmail.com>
2022-05-04 11:45:08 +0430

    add vue use and global styles

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+      
+    </header>
+
+    <main>
+        <h2 class="text-2xl text-red-500">hello</h2>
+    </main>
+</template>
+
+<style>
+@import './assets/base.css';
+
+</style>
