66bc8ccc8c6a55e778fb586cabbf756ff10168ce
abolfazl-hue <abolfazljdst@gmail.com>
2022-05-04 12:36:15 +0430

    add gmail box data in main page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
+import { gmailData } from './stores/mainData'
+const gmailBox = gmailData()
+
 </script>
 
 <template>
-    <h1>Vmail box</h1>
+    <h1 class="text-2xl mb-5">Vmail box</h1>
 
-    <table class="max-w-1000px m-auto border-collapse">
+    <table class="max-w-1000px m-auto border-collapse border-t-2 border-black">
         <tbody>
-            <tr class="h-10">
+            <tr 
+            class="h-10"
+            v-for="email in gmailBox.gmails"
+            :key="email.id"
+            :class="['cursor-pointer', email.read ? 'bg-gray-300' : '']"
+            @click="email.read = true">
                 <td class="table-items">
                     <input type="checkbox">
                 </td>
+                <td class="table-items"> {{ email.from }}</td>
                 <td class="table-items">
-                    <p class="overflow-y-hidden m-0 max-h-1.2em"><strong></strong></p>
+                    <p class="overflow-y-hidden m-0 max-h-1.2em"><strong> {{ email.subject.slice(0 , 40) }} ... </strong> - {{ email.body }}</p>
                 </td>
-                <td class="table-items"></td>
+                <td class="table-items"> {{ email.sentAt }} </td>
                 <td class="table-items w-120px">
-                    <button></button>
+                    <button @click="email.archived = true">Archive</button>
                 </td>
             </tr>
         </tbody>
