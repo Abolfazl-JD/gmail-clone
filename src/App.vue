7c5bb04c6710f0e3e454e8e53053e458b5731e01
abolfazl-hue <abolfazljdst@gmail.com>
2022-05-11 16:17:24 +0430

    delete stuff related to the selected property app.vue component

--- src/App.vue
+++ src/App.vue
@@ -6,13 +6,10 @@ import SkeletonMailTable from './components/SkeletonMailTable.vue'
 // stores
 import { gmailData } from './stores/mainData'
 const gmailBox = gmailData()
-
-const pluralSelected = computed(() => gmailBox.selectedGmails.length === 1 ? 'email' : 'emails')
 </script>
 
 <template>
     <h1 class="text-2xl mb-5">Vmail box</h1>
-    <h2 class="text-xl mb-5"> {{ gmailBox.selectedGmails.length }} {{ pluralSelected }} selected </h2>
     <Suspense>
         <template #default>
             <MailTable />
