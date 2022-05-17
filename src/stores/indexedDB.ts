541d92c86efbb85de6655120a2dfbb46c8836d2b
abolfazl-hue <abolfazljdst@gmail.com>
2022-05-17 11:31:42 +0430

    create indexedDB pinia store

---
+++ src/stores/indexedDB.ts
@@ -0,0 +1,83 @@
+import { defineStore } from "pinia"
+import type { Gmail } from '../types'
+
+export const indexedDb = defineStore({
+    id: "database",
+    
+    state: () => ({
+        database: null as null | IDBDatabase
+    }),
+    
+    actions: {
+        async getDatabase() : Promise<IDBDatabase> {
+            return new Promise((resolve, reject) => {
+                if (this.database) {
+                    resolve(this.database)
+                }
+
+                let request = window.indexedDB.open('gmailBox', 1)
+                request.onerror = event => {
+                    console.error('ERROR: unable to open the database', event)
+                    reject('ERROR')
+                }
+                request.onsuccess = () => {
+                    this.database = request.result
+                    resolve(this.database)
+                }
+  
+                request.onupgradeneeded = () => {
+                    let database = request.result
+                    database.createObjectStore('gmailBox', {
+                        autoIncrement: true,
+                        keyPath : 'id'
+                    })
+                }
+            })
+        },
+  
+        async getGmailsStore() : Promise<Gmail[]>{
+            this.database = await this.getDatabase()
+            return new Promise((resolve, reject) => {
+                if (this.database) {
+                    const transaction = this.database.transaction('gmailBox', 'readonly')
+                    const store = transaction.objectStore('gmailBox')
+  
+                    let gmailBox : Gmail[] = [] 
+                    store.openCursor().onsuccess = (event: any) => {
+                        const cursor = event.target.result
+                        if (cursor) {
+                            const gmail : Gmail = cursor.value
+                            gmailBox.push(gmail)
+                            cursor.continue()
+                        }
+                    }
+  
+                    transaction.oncomplete = () => {
+                        resolve(gmailBox)
+                    }
+                    transaction.onerror = (event) => {
+                        reject(event)
+                    }
+                }
+            })
+        },
+  
+        async saveData(gmail : Gmail) {
+            this.database = await this.getDatabase()
+            return new Promise((resolve, reject) => {
+                if (this.database) {
+                    const transaction = this.database.transaction('gmailBox', 'readwrite')
+                    const store = transaction.objectStore('gmailBox')
+                    store.put({...gmail})
+  
+                    transaction.oncomplete = () => {
+                        resolve('item has been successfully saved')
+                    }
+                    transaction.onerror = (event) => {
+                        reject(event)
+                    }
+                }
+            })
+        }
+    }
+})
