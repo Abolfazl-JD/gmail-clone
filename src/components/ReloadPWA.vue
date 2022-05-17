9aa9c598c0d7e345abc372052902d2d503e6001c
abolfazl-hue <abolfazljdst@gmail.com>
2022-05-17 13:28:59 +0430

    create beautiful styles to reload pwa component

--- src/components/ReloadPWA.vue
+++ src/components/ReloadPWA.vue
@@ -15,69 +15,70 @@ const updateSW = async () => {
 <template>
     <div 
     v-if="offlineReady || needRefresh" 
-    class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50">
-        <div class="relative top-[60px] mx-auto sm:p-5 py-2 border  shadow-lg rounded-md bg-white w-11/12 md:w-3/4">
+    class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50 flex items-end">
+        <div class="relative bottom-[60px] mx-auto sm:p-5 py-2 border  shadow-lg rounded-md bg-white w-11/12 md:w-3/4">
             <div
             class="
             flex flex-wrap
             md:flex-nowrap
-            bg-pink-900
-            text-white text-sm
+            text-gray-900
             px-6
             pt-2
             pb-3
             justify-between
             items-center
-            align-middle"
-            role="alert">
+            align-middle
+            "
+            role="alert"
+        >
             <div class="message mt-1">
-                <span v-if="offlineReady"> App ready to work offline </span>
-                <span v-else>
-                    New content available, click on reload button to update.
-                </span>
+            <span v-if="offlineReady"> App ready to work offline </span>
+            <span v-else>
+                New content available, click on reload button to update.
+            </span>
             </div>
-                <div class="buttons flex align-middle mt-2 md:mt-0">
-                    <button
-                    v-if="needRefresh"
-                    @click="updateSW()"
-                    class="
-                    w-full
-                    px-4
-                    py-2
-                    text-sm text-white
-                    leading-none
-                    transition-colors
-                    duration-150
-                    bg-pink-900
-                    border border-white
-                    rounded
-                    sm:w-auto
-                    active:bg-pink-600
-                    hover:bg-pink-700
-                    focus:outline-none focus:shadow-outline-purple
-                    mr-4">
-                        Reload
-                    </button>
-                    <button
-                    @click="close"
-                    class="
-                    w-full
-                    px-4
-                    py-2
-                    text-sm text-white
-                    leading-none
-                    transition-colors
-                    duration-150
-                    bg-pink-900
-                    border border-white
-                    rounded
-                    sm:w-auto
-                    active:bg-pink-600
-                    hover:bg-pink-700
-                    focus:outline-none focus:shadow-outline-purple">
-                        Close
-                    </button>
-                </div>
+            <div class="buttons flex align-middle mt-2 md:mt-0">
+            <button
+                v-if="needRefresh"
+                @click="updateSW()"
+                class="
+                w-full
+                px-4
+                py-2
+                text-sm 
+                leading-none
+                transition-colors
+                duration-150
+                border
+                border-gray-900
+                rounded
+                sm:w-auto
+                focus:outline-none focus:shadow-outline-purple
+                mr-4
+                "
+            >
+                Reload
+            </button>
+            <button
+                @click="close"
+                class="
+                w-full
+                px-4
+                py-2
+                text-sm 
+                leading-none
+                transition-colors
+                duration-150
+                border
+                border-gray-900 
+                rounded
+                sm:w-auto
+                focus:outline-none focus:shadow-outline-purple
+                "
+            >
+                Close
+            </button>
+            </div>
             </div>
         </div>
     </div>
